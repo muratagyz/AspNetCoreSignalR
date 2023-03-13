5deb363b5bb5c288d25e69ae2f5d0cbe4824c2c9
Murat AĞYÜZ <57285600+muratagyz@users.noreply.github.com>
2023-03-13 12:24:51 +0300

    Server Strongly typed Hubs

---
+++ SignalR/SignalR.API/Hubs/IProductHub.cs
@@ -0,0 +1,8 @@
+﻿using SignalR.API.Models;
+
+namespace SignalR.API.Hubs;
+
+public interface IProductHub
+{
+    Task ReceiveProduct(Product p);
+}
---
+++ SignalR/SignalR.API/Hubs/ProductHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.API.Models;
+
+namespace SignalR.API.Hubs;
+
+public class ProductHub : Hub<IProductHub>
+{
+    public async Task SendProduct(Product p)
+    {
+        await Clients.All.ReceiveProduct(p);
+    }
+}
